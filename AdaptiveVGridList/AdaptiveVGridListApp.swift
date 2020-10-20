73c3e10508cdd0466b4e4a75664b9d479d317737
Max <maxim.macari.dam@gmail.com>
2020-10-20 15:31:18 +0200

    Initial Commit

---
+++ AdaptiveVGridList/AdaptiveVGridListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptiveVGridListApp.swift
+//  AdaptiveVGridList
+//
+//  Created by Maxim Macari on 20/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct AdaptiveVGridListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
